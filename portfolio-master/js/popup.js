7f08d432c9c8d9f59274429474a7cb97263cf51f
unknown <yassine_badri95@outlook.fr>
2020-04-16 15:16:39 +0100

    Added responsiveness

---
+++ portfolio-master/js/popup.js
@@ -0,0 +1,16 @@
+(function ($) {
+    "use strict";
+    
+    /*--------------------------------------
+        Magnific popup Active
+    ----------------------------------------*/
+    $('.card').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    });
+    
+    })(jQuery);	
+    
